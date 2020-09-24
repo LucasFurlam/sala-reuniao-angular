0797b99d73c61ad78b8294944c1b683593e6c8e5
LucasFurlam <lucasfurlanfreitas@gmail.com>
2020-09-24 17:17:26 -0300

    Deploy do Aplicação do CRUD de Sala de Reunião

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent,
+    UpdateRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
